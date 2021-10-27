5f0b89b0e7c8312727cff4f03fae34e876c8f950
Manish Jayan <manishjayan@gmail.com>
2021-10-27 13:13:19 +0530

    modularized data

--- userData/data.js
+++ userData/data.js
@@ -3,7 +3,7 @@ var data = {
     "lastName": "Jayan",
     "city": "Thrissur",
     "state": "Kerala",
-    "phoneNumber": "8281577130",
+    "phoneNumber": "+91 828 157 7130",
     "email": "manishjayan@gmail.com",
     "website": "https://manishjayan.in",
     "github": "https://github.com/manishjayan",
@@ -82,13 +82,13 @@ var data = {
             "name": "Linux Package Management & Kernel Conﬁguration",
             "issuingOrganization": "Government College Of Engineering Kannur",
             "issuedDate": "05-03-2018",
-            "verify": ""
+            "verify": "#"
         },
         {
             "name": "Machine Learning Workshop",
             "issuingOrganization": "iitmadras",
             "issuedDate": "10-2018",
-            "verify": ""
+            "verify": "#"
         },
         {
             "name": "Architecting with Google Compute Engine",
@@ -104,7 +104,7 @@ var data = {
             "issuedDate": "2018",
             "event": "College Arts Festival",
             "placed": "Second",
-            "verify": ""
+            "verify": "#"
         },
         {
             "name": "Script Writing English",
@@ -112,7 +112,7 @@ var data = {
             "issuedDate": "2018",
             "event": "College Arts Festival",
             "placed": "Third",
-            "verify": ""
+            "verify": "#"
         },
         {
             "name": "Skit English",
@@ -178,80 +178,3 @@ var data = {
         "Travelling"
     ]
 };
-
-(function () {
-
-    let fullName = data.firstName + " " + data.lastName;
-    document.title = fullName;
-    let fullNameClass = document.querySelectorAll('.fullName');
-    fullNameClass.forEach(function (c) {
-        c.innerHTML = fullName;
-    });
-
-    let twitterClass = document.querySelectorAll('.twitter');
-    twitterClass.forEach(function (c) {
-        c.href = data.twitter;
-    });
-
-    document.getElementsByClassName("twitter")[0].href = data.twitter;
-    document.getElementsByClassName("instagram")[0].href = data.instagram;
-    document.getElementsByClassName("github")[0].href = data.github;
-    document.getElementsByClassName("linkedin")[0].href = data.linkedin;
-
-    for (var i = 0; i < 3; i++) {
-        document.getElementById("education-items").innerHTML +=
-            "<div class='resume-item'><h4 id = 'school-" + (i + 1) + "'>" + data.education[i].school
-            + "</h4><h5 id = 'period-" + (i + 1) + "'>" + data.education[i].startYear + " - " + data.education[i].endYear
-            + "</h5><em><p id = 'fieldofstudy-" + (i + 1) + "'>" + data.education[i].fieldOfStudy
-            + "</p></em><p></p></div>";
-    }
-
-    for (var i = 0; i < 4; i++) {
-        document.getElementById("professional-items").innerHTML +=
-            "<div class='resume-item'>"
-            + "<h4 id = 'etitle-" + (i + 1) + "'>" + data.experience[i].title
-            + "</h4><h5 id = 'eperiod-" + (i + 1) + "'>" + data.experience[i].startDate + " - " + data.experience[i].endDate
-            + "</h5><em><p id = 'location-" + (i + 1) + "'>" + data.experience[i].location
-            + "</h5><em><p id = 'description-" + (i + 1) + "'>" + data.experience[i].description + "</p></em></div>";
-    }
-    var c = 1;
-    for (var i = 0, j = 0; i < 3 || j < 3; i++, j++) {
-        if (i < 3) {
-            document.getElementById("certificates").innerHTML += "<div class='col-lg-4 col-md-6 portfolio-item filter-app'>"
-                + "<div class='portfolio-wrap'>"
-                + "<img src='assets/img/portfolio/portfolio-" + c + ".jpg' class='img-fluid' alt=''>"
-                + "<div class='portfolio-info'>"
-                + "<h4 id = 'technical-certificate-name-1'>" + data.technicalCertificates[i].name + "</h4>"
-                + "<p id = 'technical-certificate-issuingOrganization-1'>" + data.technicalCertificates[i].issuingOrganization + "</p>"
-                + "<p id = 'technical-certificate-issuedDate-1'>" + data.technicalCertificates[i].issuedDate + "</p>"
-                + "<div class='portfolio-links'>"
-                + "<a href='assets/img/portfolio/portfolio-1.jpg' data-gallery='portfolioGallery' class='portfolio-lightbox' title='App 1'><i class='bx bx-plus'></i></a>"
-                + "<a href='portfolio-details.html' class='portfolio-details-lightbox' data-glightbox='type: external' title='Portfolio Details'><i class='bx bx-link'></i></a>"
-                + "</div></div></div></div>";
-            c++;
-        }
-        if (j < 3) {
-            document.getElementById("certificates").innerHTML += "<div class='col-lg-4 col-md-6 portfolio-item filter-web'>"
-                + "<div class='portfolio-wrap'>"
-                + "<img src='assets/img/portfolio/portfolio-" + c + ".jpg' class='img-fluid' alt=''>"
-                + "<div class='portfolio-info'>"
-                + "<h4 id = 'technical-certificate-name-1'>" + data.otherCertificates[j].name + "</h4>"
-                + "<p id = 'technical-certificate-issuingOrganization-1'>" + data.otherCertificates[j].issuingOrganization + "</p>"
-                + "<p id = 'technical-certificate-issuedDate-1'>" + data.otherCertificates[j].issuedDate + "</p>"
-                + "<div class='portfolio-links'>"
-                + "<a href='assets/img/portfolio/portfolio-1.jpg' data-gallery='portfolioGallery' class='portfolio-lightbox' title='App 1'><i class='bx bx-plus'></i></a>"
-                + "<a href='portfolio-details.html' class='portfolio-details-lightbox' data-glightbox='type: external' title='Portfolio Details'><i class='bx bx-link'></i></a>"
-                + "</div></div></div></div>";
-            c++;
-        }
-    }
-
-    for (var i = 0; i < 3; i++) {
-        document.getElementById("project-items").innerHTML += "<div class='col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0' data-aos='zoom-in' data-aos-delay='200'>"
-            + "<div class='icon-box iconbox-orange '>"
-            + "<h4><a href='" + data.projects[i].link + "'target='_blank'>" + data.projects[i].name + " - " + data.projects[i].purpose + "</a></h4><p>"
-            + data.projects[i].description
-            + "</p></div></div>";
-    }
-
-})()
